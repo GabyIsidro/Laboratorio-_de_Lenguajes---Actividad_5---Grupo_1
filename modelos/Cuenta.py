ce9df8f494ed41d7e6ae0c772f75a905987117c5
Ramiroa222 <ranavitarte@alumnos.unsada.edu.ar>
2026-04-14 13:54:10 -0300

    Refactor class name and constructor in Cuenta.py

---
+++ modelos/Cuenta.py
@@ -0,0 +1,6 @@
+class Cuenta:
+  def __init__(self, nombre_de_usuario, mail, servidor_de_entrada, servidor_de_salida):
+    self.nombre_de_usuario = nombre_de_usuario
+    self.mail = mail
+    self.servidor_de_entrada = servidor_de_entrada
+    self.servidor_de_salida = servidor_de_salida
