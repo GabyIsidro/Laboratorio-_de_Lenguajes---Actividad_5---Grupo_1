9f0348b16474ef6f6a2c29a350d0b41d6d7a5bee
Edu498 <eaparodiocampo@alumnos.unsada.com.ar>
2026-04-14 00:06:06 -0300

    cambios en la clase, impementacion de validacion de mail

--- modelos/Contacto.py
+++ modelos/Contacto.py
@@ -1,5 +1,21 @@
 class Contacto:
     def __init__(self,nombre, apellido, mail):
+        #aplica al contacto el nombre ingresado por parametro
         self.nombre = nombre
+
+        #aplica al contacto el apellido ingresado por parametro
         self.apellido = apellido
-        self.mail = mail
+
+        #valida el mail ingresado por parametro
+        for i in (mail):
+            if i == "@":
+                arroba += 1
+            elif (i== ".") :
+                punto = True
+        if (arroba == 1 and punto) :
+            usuario, dominio = mail.split("@")
+            if usuario and dominio:
+                #aplica al contacto el mail ingresado por parametro
+                self.mail = mail
+        raise ValueError ("el mail no es valido")
+        
